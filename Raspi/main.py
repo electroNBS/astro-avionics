f5b79b44c777dbb09ad23b2b46cf8c32073e3946
MythicalNewt <89124240+MythicalNewt@users.noreply.github.com>
2025-03-13 03:56:14 +0530

    Implemented deployment charges code.

--- Raspi/main.py
+++ Raspi/main.py
@@ -7,6 +7,7 @@ import os
 import board
 import busio
 import adafruit_bmp390
+import RPi.GPIO as GPIO
 # pip install adafruit-circuitpython-bmp390
 # enable i2c and serial in raspi config
 
@@ -18,6 +19,8 @@ LOG_FILE = os.path.join(log_dir, time.strftime("arduino_%H%M%S.log"))
 TIMEOUT = 20
 SERIAL_PORT = "/dev/serial0"
 BAUD_RATE = 115200
+DROGUE_PIN = 27
+MAIN_PIN = 17
 
 # global variables
 ard_status = True
@@ -25,6 +28,11 @@ ground_alt = 0.0
 current_alt = 0.0
 height = 0.0
 last_packet_time = time.time()
+
+# GPIO init
+GPIO.setmode(GPIO.BOARD) # Check if we are using board numbering
+GPIO.setup(DROGUE_PIN, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(MAIN_PIN, GPIO.OUT, initial=GPIO.LOW)
 
 # bmp init
 i2c = busio.I2C(board.SCL, board.SDA)
@@ -39,6 +47,18 @@ def start_recording():
 
 def stop_recording():
     subprocess.run(["bash", "/home/pi/stop_recording.sh"])
+
+def deploy_drogue():
+    GPIO.output(DROGUE_PIN, GPIO.HIGH)
+    time.sleep(2)
+    GPIO.output(DROGUE_PIN, GPIO.LOW)
+    write_log("Drogue chute deployed!")
+
+def deploy_main():
+    GPIO.output(MAIN_PIN, GPIO.HIGH)
+    time.sleep(2)
+    GPIO.output(MAIN_PIN, GPIO.LOW)
+    write_log("Main chute deployed!")
 
 def write_log(data):
     global last_packet_time
@@ -100,9 +120,9 @@ async def flight_mode():
         await asyncio.sleep(0.2)
         if current_alt < previous_alt:
             counter += 1
-        if counter > 2:
+        if counter > 3:
             write_log("Descent detected! Deploying drogue chute.")
-            # Add drogue charges code here
+            deploy_drogue()
             break
         previous_alt = current_alt
         
@@ -113,7 +133,8 @@ async def descent_mode():
             await asyncio.sleep(0.5)
             continue
         write_log("500m reached, deploying main chute!")
-        # Add main chute charges code here
+        deploy_main()
+
 
 async def main():
     start_recording()
