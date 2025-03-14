66828a439e507cd8ecc9836aa0571845399d489c
MythicalNewt <89124240+MythicalNewt@users.noreply.github.com>
2025-03-14 14:31:08 +0530

    Revised apogee detection, added status led control.

--- Raspi/main.py
+++ Raspi/main.py
@@ -18,14 +18,9 @@ os.makedirs(log_dir, exist_ok=True)  # Ensure the directory exists
 # bmp init
 i2c = busio.I2C(board.SCL, board.SDA)
 bmp = adafruit_bmp390.BMP390_I2C(i2c, address=0x77) # install i2c tools and verify address with $ i2cdetect -y 1
-bmp.pressure_oversampling = 8
-bmp.temperature_oversampling = 4
-bmp.sea_level_pressure = 1007 # Set this before flight
-
-# GPIO init
-GPIO.setmode(GPIO.BOARD) # Check if we are using board numbering
-GPIO.setup(DROGUE_PIN, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(MAIN_PIN, GPIO.OUT, initial=GPIO.LOW)
+#bmp.pressure_oversampling = 8
+#bmp.temperature_oversampling = 4
+bmp.sea_level_pressure = 1007 # Set this before flight!
 
 # constants
 LOG_FILE = os.path.join(log_dir, time.strftime("arduino_%H%M%S.log"))
@@ -34,11 +29,19 @@ SERIAL_PORT = "/dev/serial0"
 BAUD_RATE = 115200
 DROGUE_PIN = 27
 MAIN_PIN = 17
-DROGUE_VEL = -5 # This should be negative!
-MAIN_VEL = -20 # This should be negative!
+STATUS = 25
+TEL = 24
+MAIN_VEL = -40 # This should be negative!
 MAIN_HEIGHT = 500
 FLIGHTMODE_ALT = 200
 GROUND_ALT = bmp.altitude
+
+# GPIO init
+GPIO.setmode(GPIO.BCM) 
+GPIO.setup(DROGUE_PIN, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(MAIN_PIN, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(STATUS, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(TEL, GPIO.OUT, initial=GPIO.LOW)
 
 # global variables
 ard_status = True
@@ -54,36 +57,38 @@ def start_recording():
     if os.path.exists(script_path):
         subprocess.run(["bash", script_path])
     else:
-        write_log("ERROR: start_recording.sh not found!")
+         write_to_file("ERROR: start_recording.sh not found!")
 
 def stop_recording():
     script_path = "/home/pi/stop_recording.sh"
     if os.path.exists(script_path):
         subprocess.run(["bash", script_path])
     else:
-        write_log("ERROR: stop_recording.sh not found!")
+        write_to_file("ERROR: stop_recording.sh not found!")
 
 async def deploy_drogue():
     GPIO.output(DROGUE_PIN, GPIO.HIGH)
     await asyncio.sleep(2)
     GPIO.output(DROGUE_PIN, GPIO.LOW)
-    write_log("Drogue chute deployed!")
+    await write_log("Drogue chute deployed!")
 
 async def deploy_main():
     GPIO.output(MAIN_PIN, GPIO.HIGH)
     await asyncio.sleep(2)
     GPIO.output(MAIN_PIN, GPIO.LOW)
-    write_log("Main chute deployed!")
+    await write_log("Main chute deployed!")
 
-def write_log(data):
+async def write_log(data):
     global last_packet_time
-    with open(LOG_FILE, "a") as file:
-        file.write(f"{time.strftime('%H%M%S')} - {data}\n")
-        file.flush()
+    await asyncio.to_thread(write_to_file, data)
     last_packet_time = time.monotonic()
 
+def write_to_file(data):
+    with open(LOG_FILE, "a") as file:
+        file.write(f"{time.strftime('%H:%M:%S')} - {data}\n")
+        file.flush()
+
 async def read_serial(aios):
-    global last_packet_time, ard_status
     while ard_status:
         try:
             data = await aios.readline_async()
@@ -91,28 +96,29 @@ async def read_serial(aios):
                 continue
 
             data = data.decode().strip()
-            write_log(data)
+            await write_log(data)
             if data == "STOPREC":
                 stop_recording()
             elif data == "EXIT":
+                await cleanup(aios)
                 sys.exit()
 
         except Exception as e:
-            write_log(f"Serial read error: {e}")
+            await write_log(f"Serial read error: {e}")
             await asyncio.sleep(0.5)
 
 async def check_failure():
-    global last_packet_time, ard_status, TIMEOUT
+    global last_packet_time, ard_status
     while ard_status:
         if time.monotonic() - last_packet_time > TIMEOUT:
-            write_log("ERROR: Arduino timed out! Switching to failure mode.")
+            await write_log("ERROR: Arduino timed out! Switching to failure mode.")
             stop_recording()
             ard_status = False
         await asyncio.sleep(1)  # Check every second
 
 async def update_vel():
     global current_alt, height, velocity, vel_logging
-    write_log(f"Started velocity loggging. Ground altitude: {GROUND_ALT}")
+    await write_log(f"Started velocity loggging. Ground altitude: {GROUND_ALT}")
     previous_alt = bmp.altitude
     previous_time = time.monotonic()
     delta_alt = 0.0
@@ -133,50 +139,60 @@ async def update_vel():
 
         # Write to log every second
         counter += 1
-        if counter > 10:
-            write_log(f"Altitude= {current_alt:.2f}  Height= {height:.2f}  Velocity= {velocity:.2f}")
+        if counter > 100:
+            await write_log(f"Altitude= {current_alt:.2f}  Height= {height:.2f}  Velocity= {velocity:.2f}")
             counter = 0
-        await asyncio.sleep(0.1)
-    write_log("Stopping velocity logging.")
+        await asyncio.sleep(0.005)
+    await write_log("Stopping velocity logging.")
 
 async def failure_mode():
-    for i in range(5):
-        # Add beeper code here to indicate failure
-        await asyncio.sleep(0.25)
     while height < FLIGHTMODE_ALT:
-        await asyncio.sleep(0.5)
+        await asyncio.sleep(0.25)
 
 async def flight_mode():
-    while velocity > DROGUE_VEL:
-        await asyncio.sleep(0.1)
-    write_log(f"Detected descent velocity greater than {DROGUE_VEL}m/s! Deploying drogue.")
+    counter = 0
+    while True:
+        if velocity < 0:
+            counter += 1
+        else:
+            counter = 0
+        if counter > 6:
+            break
+        await asyncio.sleep(0.005)
+    await write_log("Apogee detected! Deploying drogue.")
     await deploy_drogue()
         
 async def descent_mode():
     while height > MAIN_HEIGHT and velocity < MAIN_VEL:
         await asyncio.sleep(0.1)
-    write_log(f"{MAIN_HEIGHT}m reached, deploying main chute!")
+    await write_log(f"{MAIN_HEIGHT}m reached, deploying main chute!")
     await deploy_main()
 
 async def cleanup(aios):
     global vel_logging
     vel_logging = False
+    GPIO.output(STATUS, GPIO.LOW)
+    GPIO.output(TEL, GPIO.LOW)
     GPIO.cleanup()
     await aios.close()
-    write_log("Cleaned up serial and GPIO, and stopped velocity logging.")
+    await write_log("Cleaned up serial and GPIO, and stopped velocity logging.")
 
 async def main():
+    GPIO.output(STATUS, GPIO.HIGH)
+    GPIO.output(TEL, GPIO.HIGH)
     start_recording()
     aios = aioserial.AioSerial(port=SERIAL_PORT, baudrate=BAUD_RATE, timeout=1)
     await asyncio.gather(read_serial(aios), check_failure()) # make sure this function returns if ard_status is false
     
     # Failure mode
-    write_log("Entered failure mode.")
-    asyncio.create_task(update_vel())  # Add some case to stop this fn
+    await write_log("Entered failure mode.")
+    GPIO.output(TEL, GPIO.LOW)
+    vel_task = asyncio.create_task(update_vel())
     await failure_mode()
     await flight_mode()
     await descent_mode()   # Add safety cases like if velocity is greater than threshold deploy main anyways
     await cleanup(aios)    # If something needs to be done below MAIN_HEIGHT, add it above this
+    await vel_task
 
 if __name__ == "__main__":
     asyncio.run(main())
