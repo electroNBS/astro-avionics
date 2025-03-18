61d7339495da0b4817d97da904c22d196f5cb4d5
electroNBS <pcv.saisreekar@gmail.com>
2025-03-18 14:35:32 +0530

    updated IMU test code

--- ComponentTests/IMU.ino
+++ ComponentTests/IMU.ino
@@ -1,5 +1,6 @@
 #include <Wire.h>
 #include <Adafruit_ICM20649.h>
+#include <math.h>
 
 Adafruit_ICM20649 icm;
 
@@ -7,12 +8,17 @@ struct IMUReading {
     double roll;
     double yaw;
     double pitch;
-    double acceleration;
+    double accel_x;
+    double accel_y;
+    double accel_z;
 };
 
 // Calibration variables
 float accelBiasX = 0, accelBiasY = 0, accelBiasZ = 0;
 float gyroBiasX = 0, gyroBiasY = 0, gyroBiasZ = 0;
+float velocityY = 0;
+float height = 0;
+unsigned long prevTime = 0;
 
 void calibrateIMU(int samples = 500) {
     Serial.println("Calibrating... Keep the IMU steady!");
@@ -51,17 +57,51 @@ IMUReading readIMU() {
     icm.getEvent(&accel, &gyro, &temp);
 
     IMUReading reading;
-    reading.acceleration = sqrt(pow(accel.acceleration.x - accelBiasX, 2) +
-                                pow(accel.acceleration.y - accelBiasY, 2) +
-                                pow(accel.acceleration.z - accelBiasZ, 2));
-
+    reading.accel_x = accel.acceleration.x - accelBiasX;
+    reading.accel_y = accel.acceleration.y - accelBiasY;
+    reading.accel_z = accel.acceleration.z - accelBiasZ;
     reading.roll = atan2(accel.acceleration.y - accelBiasY, accel.acceleration.z - accelBiasZ) * 180 / PI;
-    reading.pitch = atan2(-(accel.acceleration.x - accelBiasX), 
-                          sqrt(pow(accel.acceleration.y - accelBiasY, 2) + pow(accel.acceleration.z - accelBiasZ, 2))) * 180 / PI;
+    reading.pitch = atan2(-(accel.acceleration.x - accelBiasX),
+                          sqrt(pow(accel.acceleration.y - accelBiasY, 2) + pow(accel.acceleration.z - accelBiasZ, 2))) *
+                    180 / PI;
 
     reading.yaw = (gyro.gyro.z - gyroBiasZ); // Only raw value, fusion needed for accurate yaw
 
     return reading;
+}
+
+float getVelocityIMU()
+{
+    unsigned long currentTime = millis();
+    float dt = (currentTime - prevTime) / 1000.0; // Convert to seconds
+    prevTime = currentTime;
+
+    sensors_event_t accel, gyro, temp;
+    icm.getEvent(&accel, &gyro, &temp);
+
+    float accelY = accel.acceleration.y - accelBiasY; // Remove bias
+    
+    //REMOVE NOISE???
+
+    velocityY += accelY * dt; // Integrate acceleration to get velocity
+    return velocityY;
+}
+
+float getHeightIMU(){
+    unsigned long currentTime = millis();
+    float dt = (currentTime - prevTime) / 1000.0; // Convert to seconds
+    prevTime = currentTime;
+
+    sensors_event_t accel, gyro, temp;
+    icm.getEvent(&accel, &gyro, &temp);
+
+    float accelY = accel.acceleration.y - accelBiasY; // Remove bias
+    
+    //REMOVE NOISE???
+
+    velocityY += accelY * dt; // Integrate acceleration to get velocity
+    height += velocityY * dt; // Integrate velocity to get height
+    return height;
 }
 
 void setup() {
@@ -84,7 +124,9 @@ void setup() {
 void loop() {
     IMUReading reading = readIMU();
 
-    Serial.print("Acceleration: "); Serial.println(reading.acceleration);
+    Serial.print("Acceleration x: "); Serial.println(reading.accel_x);
+    Serial.print("Acceleration y: "); Serial.println(reading.accel_y);
+    Serial.print("Acceleration z: "); Serial.println(reading.accel_z);
     Serial.print("Roll: "); Serial.println(reading.roll);
     Serial.print("Yaw: "); Serial.println(reading.yaw);
     Serial.print("Pitch: "); Serial.println(reading.pitch);
